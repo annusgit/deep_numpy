832dc5bd24af9fb9c4dfc776a429182d1ca5db75
annusgit <annusgit@gmail.com>
2018-04-07 21:28:44 +0500

    recurrent forward feed test working

---
+++ residual_net.py
@@ -0,0 +1,116 @@
+
+
+
+
+"""
+    A small fully connected residual net test with three residual connections
+"""
+
+from __future__ import print_function
+from __future__ import division
+
+# from graph_and_ops import GRAPH
+import numpy as np
+from graph_and_ops import GRAPH
+from Operations import placeholder, add
+from layers import Dense, Relu, Softmax
+from utils import Data
+from loss_functions import CrossEntropyLoss
+
+
+def main():
+
+    # generate some dummy data for testing
+    manager = Data()
+    num_examples = 10**4
+    max_val = 1
+    train_batch_size = 16
+    train_size = int(num_examples/2)
+    eval_size = int(num_examples/2)
+    X, y = manager.create_data_set(num_of_examples=num_examples,
+                                   max_val=max_val,
+                                   discriminator=lambda x: max_val*(1/(1+np.exp(-x)) + 1/(1+np.exp(x**2)))-max_val/2,
+                                   one_hot=False,
+                                   plot_data=False,
+                                   load_saved_data=True,
+                                   filename='dataset.npy')
+
+    train_examples, train_labels = X[0:train_size, :], y[0:train_size]
+    eval_examples, eval_labels = X[train_size:, :], y[train_size:]
+    print('train examples = {}, train labels = {}, eval examples = {}, eval labels = {}'.format(train_examples.shape,
+                                                                                                train_labels.shape,
+                                                                                                eval_examples.shape,
+                                                                                                eval_labels.shape))
+
+    # get some small train batch
+    indices = np.random.randint(low=0,high=train_size,size=train_batch_size)
+    train_batch_examples, train_batch_labels = X[indices,:], y[indices]
+
+
+    # start by defining your default graph
+    graph = GRAPH()
+    graph.getDefaultGraph()
+
+
+    # declare your placeholders, to provide your inputs
+    # print(int(train_batch_examples.shape[1]))
+    input_features = placeholder(shape=(train_batch_size, int(train_batch_examples.shape[1])))
+    input_labels = placeholder(shape=(train_batch_size))
+
+
+    """
+        Method #3
+    """
+
+    # this is defined using layers
+    layer1 = Dense(features=input_features, units=32)
+    layer1 = Relu(layer1)
+    layer2 = Dense(features=layer1, units=64)
+    layer2 = Relu(layer2)
+    layer2_1 = Dense(features=layer2, units=64)
+    layer2_1 = Relu(layer2_1)
+    layer2_2 = Dense(features=layer2_1, units=64)
+    layer2_2 = Relu(layer2_2)
+
+    # a recurrent connection
+    layer2_2 = add(layer2_2, layer2)
+
+    layer3 = Dense(features=layer2_2, units=128)
+    layer3 = Relu(layer3)
+    layer4 = Dense(features=layer3, units=128)
+    layer4 = Relu(layer4)
+    layer5 = Dense(features=layer4, units=128)
+    layer5 = Relu(layer5)
+
+    # a recurrent connection
+    layer5 = add(layer5, layer3)
+
+    layer6 = Dense(features=layer5, units=64)
+    layer6 = Relu(layer6)
+    layer6_1 = Dense(features=layer6, units=64)
+    layer6_1 = Relu(layer6_1)
+    layer6_2 = Dense(features=layer6_1, units=64)
+    layer6_2 = Relu(layer6_2)
+
+    # a recurrent connection
+    layer6_2 = add(layer6_2, layer6)
+
+    layer7 = Dense(features=layer6_2, units=32)
+    layer7 = Relu(layer7)
+    layer8 = Dense(features=layer7, units=2)
+
+    # define the logits and the loss
+    logits = Softmax(layer8)
+    loss = CrossEntropyLoss(softmax_logits=logits, labels=input_labels)
+
+
+    # compile and run
+    graph.graph_compile(function=loss, verbose=True)
+    loss = graph.run(input_matrices={input_features: train_batch_examples, input_labels: train_batch_labels})
+    print(loss, logits.output.shape)
+
+pass
+
+
+if __name__ == "__main__":
+    main()
